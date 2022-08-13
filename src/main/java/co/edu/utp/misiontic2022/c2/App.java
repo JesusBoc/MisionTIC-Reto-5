0728653024a4ef2428822a642ab9cc3f62f7a288
Jesús Bocanegra <jesusbodc@gmail.com>
2022-08-13 16:24:24 -0500

    Listo!

--- src/main/java/co/edu/utp/misiontic2022/c2/App.java
+++ src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -1,10 +1,6 @@
 package co.edu.utp.misiontic2022.c2;
 
 import co.edu.utp.misiontic2022.c2.controller.MainController;
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
